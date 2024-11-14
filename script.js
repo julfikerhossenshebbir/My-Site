50d8d2a9a0f990912559a78c75bb317bbcbae4d6
Md. Julfiker Hossen Shebbir <julfikerhossenshebbir@gmail.com>
2024-11-14 20:59:32 +0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', function () {
-    setTimeout(function () {
-        document.getElementById('preloader').style.display = 'none';
-        document.body.classList.remove('loading');
-    }, 1500);
-});
-
-document.body.classList.add('loading');
