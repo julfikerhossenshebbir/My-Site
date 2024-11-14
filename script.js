fd9b6679b634f53fc4a040e5c7b0af3452998941
Md. Julfiker Hossen Shebbir <julfikerhossenshebbir@gmail.com>
2024-11-14 17:19:57 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function () {
     setTimeout(function () {
         document.getElementById('preloader').style.display = 'none';
         document.body.classList.remove('loading');
-    }, 2000);
+    }, 1500);
 });
 
 document.body.classList.add('loading');
